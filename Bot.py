4318f49fb7bca63fd66e77b2985c2b97ae73bb83
linobigatti <linobigatti2015@gmail.com>
2018-11-05 17:52:20 -0300

    Oops. Little fix

--- Bot.py
+++ Bot.py
@@ -11,22 +11,15 @@ config = json.load(config_raw)
 
 TOKEN = config["token"]
 
-bot.run(TOKEN)
-
 @bot.event
 async def on_ready():
 	print('Coolbot++ - a bot by L!no')
 	print('Logged in as')
-	print(client.user.name)
-	print(client.user.id)
+	print(bot.user.name)
+	print(bot.user.id)
 	print('------')
 
-while(1):
-	while x != 3:
-		x += 1
-		print(x)
-		time.sleep(1)
-	while x != 1:
-		x -= 1
-		print(x)
-		time.sleep(1)
+def main():
+	bot.run(TOKEN)
+
+main()
